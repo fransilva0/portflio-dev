ef568e37d507616e596e7d7c88631346b4ea2611
fransilva0 <sfrancileudo1@gmail.com>
2024-12-14 16:55:01 -0300

    feat: creating a back to top button

--- js/index.js
+++ js/index.js
@@ -49,4 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+      window.onscroll = function() {
+        var button = document.querySelector(".scroll-to-top");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          button.style.display = "block";
+        } else {
+          button.style.display = "none";
+        }
+      };
   
+      function scrollToTop() { window.scrollTo({ top: 0, behavior: 'smooth' });}
